06a6c25b8fd7d5d65cfce3b4b58654527fe07268
Diego Castillo Flores <cfdiego1996@gmail.com>
2025-07-29 20:50:58 -0300

    firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,11 @@
 // Firebase configuration for morfemalibreria-b8c79
 // Replace the values below with your project's configuration
 export default {
-  apiKey: 'AIzaSyAN2YHco-dT6uao47aEnyN_7QQ23T0JVsM',
-  authDomain: 'morfemalibreria-b8c79.firebaseapp.com',
-  projectId: 'morfemalibreria-b8c79'
+  apiKey: "AIzaSyAN2YHco-dT6uao47aEnyN_7QQ23T0JVsM",
+  authDomain: "morfemalibreria-b8c79.firebaseapp.com",
+  projectId: "morfemalibreria-b8c79",
+  storageBucket: "morfemalibreria-b8c79.firebasestorage.app",
+  messagingSenderId: "324612251176",
+  appId: "1:324612251176:web:eeb1a9a7baf5e40baddf1d",
+  measurementId: "G-56YKZS7WWL"
 };
