78e58149aa975be8b32430285fed9133bab65ff6
Diego Castillo Flores <cfdiego1996@gmail.com>
2025-07-29 21:16:29 -0300

    config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,3 @@
-// Firebase configuration for morfemalibreria-b8c79
-// Replace the values below with your project's configuration
 export default {
   apiKey: "AIzaSyAN2YHco-dT6uao47aEnyN_7QQ23T0JVsM",
   authDomain: "morfemalibreria-b8c79.firebaseapp.com",
